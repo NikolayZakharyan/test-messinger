b8c9b29e75d6ddd91d4a79d1248037b50baef435
Nikolay <nikolzakharyan@gmail.com>
2021-04-14 15:13:13 +0400

    second commit fix provider for netlify

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useRef, useEffect } from 'react';
 import { Context } from '../index';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import SingleMsg from '../components/SingleMsg';
 
 function Chat() {
   const messagesEndRef = useRef(null);
@@ -13,17 +14,20 @@ function Chat() {
     database.collection('messages').orderBy('msgTime')
   );
 
-  const sendMessage = async () => {
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
     if (message.length) {
       await database.collection('messages').add({
         id: user.uid,
         name: user.displayName,
-        msgTime: firebase.firestore.FieldValue.serverTimestamp(),
+        msgTime: await firebase.firestore.FieldValue.serverTimestamp(),
         message,
       });
       setMessage('');
     }
   };
+
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -37,38 +41,37 @@ function Chat() {
   return (
     <>
       <div className="chat">
-        <h1>CHAT</h1>
+        <h1>{`HI ${user.displayName}`}</h1>
         {!loading ? (
           <div className="chat_wrapper">
-            {messages.map(({ id, name, msgTime, message }) => {
+            {messages.map(({ id, name, msgTime, message }, i) => {
               return (
-                <div className={`container ${id === user.uid ? 'darker' : ''}`}>
-                  <p>H{message}</p>
-                  <span
-                    className={id === user.uid ? `time-right` : `time-left`}
-                  >
-                    11:00 - {name}
-                  </span>
-                </div>
+                <SingleMsg
+                  owner={id === user.uid}
+                  data={{ id, name, msgTime, message }}
+                  message={message}
+                  name={name}
+                  msgTime={msgTime}
+                  id={id}
+                  key={i}
+                />
               );
             })}
             <div ref={messagesEndRef} />
           </div>
         ) : (
-          <>loading</>
+          <div>loading</div>
         )}
 
-        <div className="input-div">
+        <form className="input-div" onSubmit={sendMessage}>
           <input
             ref={inputEl}
             type="text"
             onChange={(e) => setMessage(e.target.value)}
             value={message}
           ></input>
-          <button type="button" onClick={sendMessage}>
-            Send
-          </button>
-        </div>
+          <button type="submit">Send</button>
+        </form>
       </div>
     </>
   );
